66600e6e427084e51ce1b1cd7226f7c5688fa8a2
flatsilver <ginpeimisonou@gmail.com>
2018-11-29 23:45:03 +0900

    modified dir name

--- r/eda.R
+++ r/eda.R
@@ -4,6 +4,6 @@ library(rpart)
 library(rpart.plot)
 library(randomForest)
 
-ibm <- read_csv("WA_Fn-UseC_-HR-Employee-Attrition.csv")
+ibm <- read_csv("data/WA_Fn-UseC_-HR-Employee-Attrition.csv")
 
 ggplot(ibm, aes())
