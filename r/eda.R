135ef5bd8e1087f25bc2139ee973c4a50812adb6
Gimpei Misono <flatsilver@Gimpeis-MacBook-Pro.local>
2018-12-14 08:46:59 +0900

    Add GGally plot

--- r/eda.R
+++ r/eda.R
@@ -3,6 +3,7 @@ library(scales)
 library(rpart)
 library(rpart.plot)
 library(randomForest)
+library(GGally)
 
 ibm <- read_csv("data/WA_Fn-UseC_-HR-Employee-Attrition.csv")
 
@@ -12,3 +13,21 @@ ibm %>%
     n = n(),
     percentage = n() / length(ibm$Attrition)
   )
+
+foo <- ibm %>% 
+  group_by(Attrition) %>% 
+  count()
+
+p1 <- foo %>% 
+  mutate(percentage = str_c(as.character(round(n / sum(foo$n) * 100, 1)), "%")) %>% 
+  ggplot(aes(Attrition, n, fill = Attrition)) +
+  geom_col() +
+  geom_label(aes(label = percentage), position = position_dodge(width = 1)) +
+  labs(y = "Number of people") +
+  theme(legend.position = "none")
+
+ggplot(ibm, aes(MonthlyIncome, MonthlyRate)) +
+  geom_hex()
+
+pm <- ggpairs(ibm, mapping = aes(colour = Attrition), columns = c("MonthlyIncome", "MonthlyRate", "DailyRate", "HourlyRate"))
+pm
